8c1ad785a0a38ceef52ff3f233f5a386a0e21841
jackyk <jackykimani13@gmail.com>
2018-09-11 11:04:25 +0000

    Load tasks from task collection and inserting from database

--- simple-todos__1/imports/api/tasks.js
+++ simple-todos__1/imports/api/tasks.js
@@ -1,3 +1,5 @@
 import { Mongo } from 'meteor/mongo';
 
+// Making it available to the whole environment
+ //last tasks is the name of the file that we are manipulating 
 export const Tasks = new Mongo.Collection('tasks');
--- simple-todos__1/imports/ui/body.js
+++ simple-todos__1/imports/ui/body.js
@@ -3,7 +3,7 @@ import { Template } from 'meteor/templating';
 import { Tasks } from '../api/tasks.js';
 
 import './body.html';
-
+// Using a template
 Template.body.helpers({
   tasks() {
     return Tasks.find({});
