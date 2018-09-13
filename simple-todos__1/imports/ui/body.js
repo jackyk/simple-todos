b37602c7aa39a8c534a242cd9c668dadb3733bb7
jackyk <jackykimani13@gmail.com>
2018-09-13 11:39:25 +0000

    add incomplete count helper

--- simple-todos__1/imports/ui/body.js
+++ simple-todos__1/imports/ui/body.js
@@ -9,18 +9,23 @@ import './body.html';
 Template.body.onCreated(function bodyOnCreated() {
   this.state = new ReactiveDict();
 });
-
+//pass data into the template
 Template.body.helpers({
   tasks() {
     const instance = Template.instance();
- if (instance.state.get('hideCompleted')) {
+    if (instance.state.get('hideCompleted')) {
    // If hide completed is checked, filter tasks
-   return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
- }
+   // Check the objects in the database that are not checked($ne:true ) add the top first
+      return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
+    }
  // Otherwise, return all of the tasks
     // return Tasks.find({});
     // Show newest tasks at the top
    return Tasks.find({}, { sort: { createdAt: -1 } });
+  },
+
+  incompleteCount() {
+    return Tasks.find({ checked: { $ne: true } }).count();
   },
 });
 
@@ -44,7 +49,7 @@ Template.body.events({
     target.text.value = '';
   },
 
-  'change .hide-completed input'(event, instance) {
+    'change .hide-completed input'(event, instance) {
     instance.state.set('hideCompleted', event.target.checked);
   },
 
